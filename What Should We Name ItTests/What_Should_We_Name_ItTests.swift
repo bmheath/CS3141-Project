a9539388700701f1295390888995776fed16234f
Bryan Heath <bryanheath@Bryans-MacBook-Pro.local>
2017-10-02 18:30:29 -0400

    Initial Commit

---
+++ What Should We Name ItTests/What_Should_We_Name_ItTests.swift
@@ -0,0 +1,36 @@
+//
+//  What_Should_We_Name_ItTests.swift
+//  What Should We Name ItTests
+//
+//  Created by Bryan Heath on 10/2/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import XCTest
+@testable import What_Should_We_Name_It
+
+class What_Should_We_Name_ItTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
