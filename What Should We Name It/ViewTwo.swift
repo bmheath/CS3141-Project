47f130d4587365cfeac33e746c1b45c0d5299e67
Bryan Heath <bryanheath@rover-218-26.rovernet.mtu.edu>
2017-11-13 12:10:00 -0500

    update

--- What Should We Name It/ViewTwo.swift
+++ What Should We Name It/ViewTwo.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewTwo: UIViewController {
     @IBOutlet weak var start: UIButton!
+    var deathLabel: SKLabelNode!
+    var levelLabel: SKLabelNode!
+    var death = save.integer(forKey: "death")
+    var level = save.integer(forKey: "level")
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        deathLabel = SKLabelNode(fontNamed: "Papyrus")
+        deathLabel.text = "Deaths: \(death)"
+        deathLabel.horizontalAlignmentMode = .center
+        deathLabel.position = CGPoint(x: 0, y: 50)
+        print(deathLabel)
+        
+        levelLabel = SKLabelNode(fontNamed: "Papyrus")
+        levelLabel.text = "Level: \(level)"
+        levelLabel.horizontalAlignmentMode = .center
+        levelLabel.position = CGPoint(x: 0, y: 100)
+        print(levelLabel)
+        
     }
 }
