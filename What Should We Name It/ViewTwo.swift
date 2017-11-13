77b4c75926f4dad24a8823d11c0ef4fb617e3435
Bryan Heath <bryanheath@rover-218-26.rovernet.mtu.edu>
2017-11-13 12:48:19 -0500

    new back

--- What Should We Name It/ViewTwo.swift
+++ What Should We Name It/ViewTwo.swift
@@ -11,29 +11,16 @@ import GameplayKit
 
 class ViewTwo: UIViewController {
     @IBOutlet weak var start: UIButton!
-    var deathLabel: SKLabelNode!
-    var levelLabel: SKLabelNode!
+    @IBOutlet weak var deathlab: UILabel!
+    @IBOutlet weak var levelLab: UILabel!
+    @IBOutlet weak var image: UIImageView!
     var death = save.integer(forKey: "death")
     var level = save.integer(forKey: "level")
     
-    
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        deathLabel = SKLabelNode(fontNamed: "Papyrus")
-        deathLabel.text = "Deaths: \(death)"
-        deathLabel.horizontalAlignmentMode = .center
-        deathLabel.position = CGPoint(x: 0, y: 50)
-        print(deathLabel)
-        
-        levelLabel = SKLabelNode(fontNamed: "Papyrus")
-        levelLabel.text = "Level: \(level)"
-        levelLabel.horizontalAlignmentMode = .center
-        levelLabel.position = CGPoint(x: 0, y: 100)
-        print(levelLabel)
-        
+        deathlab.text = "Deaths: \(death)"
+        levelLab.text = "Level: \(level)"
+        self.view.sendSubview(toBack: image)
     }
 }
